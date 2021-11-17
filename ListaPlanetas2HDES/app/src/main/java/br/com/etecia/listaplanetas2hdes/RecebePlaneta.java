7a0daa76ec4162c1b80fe3c1f78f7c1f5f3b385e
duddaarants <meduardaarantes66@gmail.com>
2021-11-17 20:01:52 -0300

    novo

---
+++ ListaPlanetas2HDES/app/src/main/java/br/com/etecia/listaplanetas2hdes/RecebePlaneta.java
@@ -0,0 +1,14 @@
+package br.com.etecia.listaplanetas2hdes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecebePlaneta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recebe_planeta);
+    }
+}
